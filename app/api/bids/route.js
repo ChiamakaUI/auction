33b5b1996fac021031eb6b69b28f9b59adc42ea7
LuisChiej <>
2024-02-29 13:27:56 +0100

    bids route

--- app/api/bids/route.js
+++ app/api/bids/route.js
@@ -1,4 +1,4 @@
-import { Server } from "socket.io" 
+// import { Server } from "socket.io" 
 import { NextRequest, NextResponse } from "next/server";
 
 // import { db } from "../../../lib/db"
@@ -21,31 +21,31 @@ export function GET(req, res) {
     return NextResponse.json({data: "Hello world"});
 }
 
-export const config = {
-    api: {
-      bodyParser: false,
-    },
-  }
+// export const config = {
+//     api: {
+//       bodyParser: false,
+//     },
+//   }
 
-  export default function SocketHandler(req, res) {
-    if (res.socket.server.io) {
-      res.status(200).json({ success: true, message: "Socket is already running", socket: `:${PORT + 1}` })
-      return
-    }
+//   export default function SocketHandler(req, res) {
+//     if (res.socket.server.io) {
+//       res.status(200).json({ success: true, message: "Socket is already running", socket: `:${PORT + 1}` })
+//       return
+//     }
   
-    console.log("Starting Socket.IO server on port:", PORT + 1)
-    //@ts-expect-error
-    const io = new Server({ path: "/api/socket", addTrailingSlash: false, cors: { origin: "*" } }).listen(3001)
+//     console.log("Starting Socket.IO server on port:", PORT + 1)
+//     //@ts-expect-error
+//     const io = new Server({ path: "/api/socket", addTrailingSlash: false, cors: { origin: "*" } }).listen(3001)
   
-    io.on("connect", socket => {
-      const _socket = socket
-      console.log("socket connect", socket.id)
-      _socket.broadcast.emit("welcome", `Welcome ${_socket.id}`)
-      socket.on("disconnect", async () => {
-        console.log("socket disconnect")
-      })
-    })
+//     io.on("connect", socket => {
+//       const _socket = socket
+//       console.log("socket connect", socket.id)
+//       _socket.broadcast.emit("welcome", `Welcome ${_socket.id}`)
+//       socket.on("disconnect", async () => {
+//         console.log("socket disconnect")
+//       })
+//     })
   
-    res.socket.server.io = io
-    res.status(201).json({ success: true, message: "Socket is started", socket: `:${PORT + 1}` })
-  }
+//     res.socket.server.io = io
+//     res.status(201).json({ success: true, message: "Socket is started", socket: `:${PORT + 1}` })
+//   }
